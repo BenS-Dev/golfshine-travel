d43abe3d22758cd503103a54e7fb1b104b85323f
Ben Silver <ben@wpg-plan.com>
2026-05-21 17:38:29 -0500

    File size reduction and bug fixes

--- golf 2/pie.php
+++
@@ -1,7 +0,0 @@
-<tr>
-	<td width="1000" valign="top" align="center">
-		<table width="1000" valign="top" align="left">
-			<tr><td width="1000" height="40" colspan="3"></td></tr>
-		</table>
-	</td>
-</tr>
--- index (1).php
+++
@@ -1,4 +0,0 @@
-<?php
-// Powered by Towebs.com
-include dirname(__FILE__).'/sitepro/index.php';
-?>
